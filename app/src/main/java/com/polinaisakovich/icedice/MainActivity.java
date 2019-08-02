ddb14711ce0982362ca4c8c6ab6f6b8c9cbae9c2
polinasmogi <46492599+polinasmogi@users.noreply.github.com>
2019-08-02 22:32:17 +0300

    /

--- app/src/main/java/com/polinaisakovich/icedice/MainActivity.java
+++ app/src/main/java/com/polinaisakovich/icedice/MainActivity.java
@@ -1,10 +1,10 @@
 package com.polinaisakovich.icedice;
 
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -13,16 +13,22 @@ public class MainActivity extends AppCompatActivity {
 
     Random random = new Random();
 
-    private TextView diceView;
+    Player player1 = new Player();
+    Player player2 = new Player();
 
-    private TextView score;
+    private TextView turnView;
 
-    private TextView yourScore;
+    private TextView currentScoreView; //The display of the current score (sum of all scores)
 
-    private Button btnTake;
+    private TextView firstPlayerScoreView; //The display of 1 player total score
+    private TextView secondPlayerScoreView; //The display of 2 player total score
 
-    private int currentScore = 0;
-    private int points = 0;
+    private ImageButton btnDice;
+    private Button btnHold;
+
+    private int currentScore = 0; //The sum of all scores of the round
+
+    private boolean player1Turn = true;
 
 
     @Override
@@ -30,13 +36,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        diceView = findViewById(R.id.txtDice);
+        turnView = findViewById(R.id.txtTurn);
+        turnView.setText("PLAYER 1");
 
-        score = findViewById(R.id.txtScore);
+        currentScoreView = findViewById(R.id.txtScore);
 
-        yourScore = findViewById(R.id.txtFirstPlayerScore);
+        firstPlayerScoreView = findViewById(R.id.txtFirstPlayerScore);
+        secondPlayerScoreView = findViewById(R.id.txtSecondPlayerScore);
 
-        btnTake = findViewById(R.id.btnHold);
+        btnDice = findViewById(R.id.btnDice);
+        btnHold = findViewById(R.id.btnHold);
 
     }
 
@@ -44,25 +53,57 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
 
         switch (view.getId()) {
-            case R.id.btnStart:
+            case R.id.btnDice:
                 int dice = 1 + random.nextInt(7-1);
-                diceView.setText(Integer.toString(dice));
+
+                switch (dice) {
+                    case 1:
+                        btnDice.setImageResource(R.drawable.dice_1);
+                        break;
+                    case 2:
+                        btnDice.setImageResource(R.drawable.dice_2);
+                        break;
+                    case 3:
+                        btnDice.setImageResource(R.drawable.dice_3);
+                        break;
+                    case 4:
+                        btnDice.setImageResource(R.drawable.dice_4);
+                        break;
+                    case 5:
+                        btnDice.setImageResource(R.drawable.dice_5);
+                        break;
+                    case 6:
+                        btnDice.setImageResource(R.drawable.dice_6);
+                        break;
+                }
 
                 if (dice != 6 ) {
                     currentScore += dice;
-                    score.setText(Integer.toString(currentScore));
+                    currentScoreView.setText(Integer.toString(currentScore));
                 } else {
                     currentScore = 0;
-                    score.setText(Integer.toString(currentScore));
-                    score.setText("You lose");
+                    currentScoreView.setText(Integer.toString(currentScore));
+                    player1Turn = !player1Turn;
                 }
                 break;
 
             case R.id.btnHold:
-                points += currentScore;
-                currentScore = 0;
-                score.setText(Integer.toString(currentScore));
-                yourScore.setText(Integer.toString(points));
+
+                if (player1Turn) {
+                    player1.score += currentScore;
+                    firstPlayerScoreView.setText(Integer.toString(player1.score));
+                    currentScore = 0;
+                    currentScoreView.setText(Integer.toString(currentScore));
+                    checkForWin();
+                    player1Turn = !player1Turn;
+                } else {
+                    player2.score += currentScore;
+                    secondPlayerScoreView.setText(Integer.toString(player2.score));
+                    currentScore = 0;
+                    currentScoreView.setText(Integer.toString(currentScore));
+                    checkForWin();
+                    player1Turn = !player1Turn;
+                }
                 break;
 
             default:
@@ -70,6 +111,18 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        if (player1Turn) {
+            turnView.setText("PLAYER 1");
+        } else {
+            turnView.setText("PLAYER 2");
+        }
+    }
 
+    private void checkForWin() {
+        if (player1.score >= 30) {
+            currentScoreView.setText("Player 1 wins");
+        } else if (player2.score >= 30) {
+            currentScoreView.setText("Player 2 wins");
+        }
     }
 }
---
+++ app/src/main/java/com/polinaisakovich/icedice/Player.java
@@ -0,0 +1,5 @@
+package com.polinaisakovich.icedice;
+
+public class Player {
+    int score = 0;
+}
